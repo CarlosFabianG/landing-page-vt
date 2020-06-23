fabc55255ab7423a90a675d8d647696f389654f6
Carlos Fabián <carl.fab.g@gmail.com>
2020-06-23 01:17:12 -0500

    nav__mobile funcionando

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
-const $navMenu = document.querySelector('.nav')
+const $navMenu = document.querySelector('.nav__mobile')
 const $burgerButton = document.querySelector('#burger-button')
 const $body = document.querySelector('body')
 
 
 $burgerButton.addEventListener('click', () => {
-    $navMenu.classList.toggle('nav--active')
+    $navMenu.classList.toggle('nav__mobile--active')
     $body.classList.toggle('scroll')
     $burgerButton.classList.toggle('burguer-icon--active')
 })
